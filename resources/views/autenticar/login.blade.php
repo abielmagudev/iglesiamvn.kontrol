a36271d6722468d85c2b355f7e2125953fa7dd4b
Abiel Magu <abielmagu.dev@gmail.com>
2023-09-09 06:07:11 -0500

    Refactoriza un wrapper central y design del mensaje de error de autenticacion

--- resources/views/autenticar/login.blade.php
+++ resources/views/autenticar/login.blade.php
@@ -2,36 +2,37 @@
 @section('contenido')
 <section class="hero is-primary is-bold is-fullheight">
     <div class="hero-body is-justify-content-center">
-        <div class="box has-text-centered pb-5" style="min-width: 400px">
-            <div class="title">
-                <img src="{{ asset('images/mvn-favicon-b.png') }}" width="36" height="36" />
-                <span class="has-text-dark ml-1">KONTROL</span>
+        <div id="wrapper-centered">
+            @if( $errors->any() )
+            <div class="notification is-danger has-text-centered">
+                <strong>Usuario ó contraseña incorrectos</strong>
             </div>
+            @endif
 
-            <form action="{{ route('autenticar.attempt') }}" method="post" autocomplete="off">
-                <div class="field">
-                    <div class="control">
-                        <input type="text" class="input" name="usuario" value="{{ old('usuario') }}" placeholder="Usuario" required autofocus />
-                    </div>
+            <div class="box has-text-centered pb-5" style="min-width: 400px">
+                <div class="title">
+                    <img src="{{ asset('images/mvn-favicon-b.png') }}" width="36" height="36" />
+                    <span class="has-text-dark ml-1">KONTROL</span>
                 </div>
-                <div class="field">
-                    <div class="control">
-                        <input type="password" class="input" name="contrasena" placeholder="Contraseña" required />
+    
+                <form action="{{ route('autenticar.attempt') }}" method="post" autocomplete="off">
+                    <div class="field">
+                        <div class="control">
+                            <input type="text" class="input" name="usuario" value="{{ old('usuario') }}" placeholder="Usuario" required autofocus />
+                        </div>
                     </div>
-                </div>
-                <button class="button is-primary is-fullwidth">
-                    <b>ENTRAR</b>
-                </button>
-                @csrf
-            </form>
+                    <div class="field">
+                        <div class="control">
+                            <input type="password" class="input" name="contrasena" placeholder="Contraseña" required />
+                        </div>
+                    </div>
+                    <button class="button is-primary is-fullwidth">
+                        <b>ENTRAR</b>
+                    </button>
+                    @csrf
+                </form>
+            </div>
         </div>
-
-        @if( $errors->any() )
-        <div class="notification is-danger">
-            <span>Usuario y contraseña no coinciden</span>
-        </div>
-        @endif
-        
     </div>
 </section>
 @endsection
