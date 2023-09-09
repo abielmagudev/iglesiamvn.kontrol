8dea9e1124ba735a79fc3222fdc810f8d9db4b24
Abiel Magu <abielmagu.dev@gmail.com>
2023-09-09 13:46:55 -0500

    Actualiza design de cumpleanios del mes en escritorio

--- resources/views/escritorio/index/cumpleanios-mes.blade.php
+++ resources/views/escritorio/index/cumpleanios-mes.blade.php
@@ -7,11 +7,10 @@ $index = 1;
 ?>
 
 <div class="box">
-    <div class="title">{{ now()->toFormattedDateString() }}</div>
-    <div class="subtitle has-text-grey">
-        <span>Cumpleañeros:</span>
-        <b class="has-text-dark">{{ $cumpleanieros->count() }}</b>
+    <div class="is-pulled-right">
+        <div class="subtitle">{{ now()->toFormattedDateString() }}</div>
     </div>
+    <div class="title">Hoy {{ $cumpleanieros->count() }} cumpleañero(s)</div>
     
     @if( $cumpleanieros->count() )
     <x-table class='is-fullwidth is-hoverable'>
@@ -29,13 +28,15 @@ $index = 1;
         @foreach($cumpleanieros as $miembro)           
         <tr>
             <td style="width:1%">{{ $index++ }}</td>
-            <td>
-                <a href="{{ route('miembros.show', $miembro) }}">{{ $miembro->nombre_completo }}</a>
-                <x-icons.cake />
+            <td class="has-text-nowrap">
+                <div>
+                    <a href="{{ route('miembros.show', $miembro) }}">{{ $miembro->nombre_completo }}</a>
+                    <x-icons.cake />
+                </div>
             </td>
-            <td>{{ $miembro->edad_anios }}</td>
-            <td>{{ $miembro->numero_movil }}</td>
-            <td>{{ $miembro->numero_telefono }}</td>
+            <td class="has-text-nowrap">{{ $miembro->edad_anios }}</td>
+            <td class="has-text-nowrap">{{ $miembro->numero_movil }}</td>
+            <td class="has-text-nowrap">{{ $miembro->numero_telefono }}</td>
             <td>{{ $miembro->correo_electronico }}</td>
             <td>
                 <x-custom.tag-estatus-membresia :activo="$miembro->activo" />
